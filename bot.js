ed66b190808cb464541655190d25b4da3b9e8610
ukitta555 <ukitta555@gmail.com>
2025-03-09 19:31:57 -0400

    add port for render

--- bot.js
+++ bot.js
@@ -1830,3 +1830,13 @@ bot.on('message', async (msg) => {
         console.error('Error processing message:', error);
     }
 }); 
+
+const express = require('express');
+const port = 5000;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
